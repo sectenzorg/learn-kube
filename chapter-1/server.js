836a35455261fd2e6361a74acf7ccd989769184a
Syarip Muhammad Abdillah <abdillahsyarip@gmail.com>
2022-09-07 11:52:31 +0700

    chapter-1

--- chapter-1/server.js
+++ chapter-1/server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.listen(3000, function () {
-    console.Log('Listening on port 3000!');
+    console.log('Listening on port 3000!');
     console.log('http://localhost:3000');
 });
